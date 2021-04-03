882bd154f2fca8c629af3f5f22e892eb3e0a8dc1
kh-leong <kenghoe112@yahoo.com.sg>
2021-04-03 16:55:11 +0800

    - Done with popups for editing and deleting - Shifted popups into a new component - Added loading indicator when fetching data

---
+++ src/components/TableEditModalComponent.jsx
@@ -0,0 +1,132 @@
+
+import React, { useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Modal, TextField } from '@material-ui/core';
+import firebase from "firebase/app";
+import "firebase/auth";
+import 'firebase/firestore';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function TableEditModalComponent(props) {
+  const classes = useStyles();
+  var db = firebase.firestore();
+
+  const showModal = props.showModal;
+  const handleCloseModal = props.handleCloseModal;
+  const modalType = props.modalType;
+  const modalData = props.modalData;
+  const numRef = useRef(null);
+
+  const firestoreUpdateHandler = () => {
+    db.collection("medicine").doc(modalData.name).set({
+      num: parseInt(numRef.current?.value),
+    }, { merge: true }).then(() => {
+      console.log("Updated " + modalData.name + "!");
+    })
+  };
+
+  const firestoreDeleteHandler = () => {
+    db.collection("medicine").doc(modalData.name).delete()
+    .then(() => {
+      console.log("Deleted " + modalData.name + "!");
+    })
+  };
+
+  let body;
+  if (modalType === "edit") {
+    body = (
+      <div className={classes.paper}>
+        Edit {modalData.name_cn}
+        <br/>
+        <TextField 
+          required name="num" 
+          label="Num" 
+          type="number" 
+          defaultValue={modalData.num} 
+          margin="normal"
+          inputRef={numRef}
+        />
+        <br/>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            // TODO: validation for TextField
+            firestoreUpdateHandler();
+            handleCloseModal();
+          }}
+        >
+          Edit
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={handleCloseModal}
+        >
+          Cancel
+        </Button>
+      </div>
+    );
+  } else if (modalType === "add") {
+    // TODO: body for add
+  } else if (modalType === "delete") {
+    body = (
+      <div className={classes.paper}>
+        Delete {modalData.name_cn}?
+        <br/>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            firestoreDeleteHandler();
+            handleCloseModal();
+          }}
+        >
+          Delete
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={handleCloseModal}
+        >
+          Cancel
+        </Button>
+      </div>
+    );
+  } else {
+    body = (
+      <div className={classes.paper}>
+      </div>
+    );
+  }
+
+  return (
+    <Modal 
+      open={showModal}
+      onClose={handleCloseModal}
+      className={classes.modal}>
+      <>
+        {body}
+      </>
+    </Modal>
+  );
+}
--- src/routes/Inventory.jsx
+++ src/routes/Inventory.jsx
@@ -3,24 +3,12 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TableComponent from './../components/TableComponent';
 import { FirestoreCollection } from '@react-firebase/firestore';
-import Modal from '@material-ui/core/Modal';
+import TableEditModalComponent from '../components/TableEditModalComponent';
+import { CircularProgress } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
-  },
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    position: 'absolute',
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
   },
 }));
 
@@ -29,10 +17,12 @@ export default function Inventory() {
   const classes = useStyles();
   const [showModal, setShowModal] = useState(false);
   const [modalType, setModalType] = useState(null);
+  const [modalData, setModalData] = useState(null);
 
   const handleCloseModal = () => {
     setShowModal(false);
     setModalType(null);
+    setModalData(null);
   };
 
   const onClickEdit = data => e => {
@@ -40,6 +30,7 @@ export default function Inventory() {
     console.log(data);
     setShowModal(true);
     setModalType("edit");
+    setModalData(data);
   };
 
   const onClickDelete = data => e => {
@@ -47,42 +38,17 @@ export default function Inventory() {
     console.log(data);
     setShowModal(true);
     setModalType("delete");
+    setModalData(data);
   };
-
-  let body;
-  if (modalType === "edit") {
-    body = (
-      <div className={classes.paper}>
-        Edit
-      </div>
-    );
-  } else if (modalType === "delete") {
-    body = (
-      <div className={classes.paper}>
-        Delete
-      </div>
-    );
-  } else {
-    body = (
-      <div className={classes.paper}>
-      </div>
-    );
-  }
 
   return (
     <div className={classes.margin}>
       <FirestoreCollection path="/medicine">
         {d => {
-          return (d.isLoading ? console.log("Loading") : <TableComponent data={d.value} onClickEdit={onClickEdit} onClickDelete={onClickDelete} />);
+          return (d.isLoading ? <CircularProgress/> : <TableComponent data={d.value} onClickEdit={onClickEdit} onClickDelete={onClickDelete} />);
         }}
       </FirestoreCollection>
-      <Modal
-        open={showModal}
-        onClose={handleCloseModal}
-        className={classes.modal}
-      >
-        {body}
-      </Modal>
+      <TableEditModalComponent showModal={showModal} handleCloseModal={handleCloseModal} modalType={modalType} modalData={modalData}/>
     </div>
   );
 }
