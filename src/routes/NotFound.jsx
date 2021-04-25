fb7f3987039fea7972b167cbc3defab6f8f9f932
kh-leong <kenghoe112@yahoo.com.sg>
2021-04-25 21:20:16 +0800

    Update style for NotFound page

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -5,6 +5,10 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
+    minHeight: "100vh",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
   },
 }));
 
